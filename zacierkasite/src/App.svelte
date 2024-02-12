aa7430d482154e22ec647f577b3723e0d6f24809
Mike <mczacierka@gmail.com>
2024-02-11 20:03:51 -0500

    svelte to react

--- zacierkasite/src/App.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-
-</script>
-
-<h1>Pocketbase</h1>
--- zacierkasite/src/lib/pocketbase.ts
+++
@@ -1,5 +0,0 @@
-import PocketBase from 'pocketbase';
-
-export const pb = new PocketBase('http://0.0.0.0:8080');
-
-const collection = await pb.collections.getOne('posts');
