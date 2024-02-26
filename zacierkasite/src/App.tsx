7e1b38920373a89c7b8f71ede4b15c8520f9e1f7
zacierkam <MZacierka@asrcfederal.com>
2024-02-25 21:51:32 -0500

    basic linktree

--- zacierkasite/src/App.tsx
+++ zacierkasite/src/App.tsx
@@ -1,12 +1,15 @@
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import LinkTree from './pages/LinkTree'
 import NotFound from './pages/NotFound'
+
 
 function App() {
   return (
       <Routes>
         <Route  path="/" element={<Home />} />
         <Route  path="*" element={<NotFound />} />
+        <Route path='links' element={<LinkTree />}/>
       </Routes>
   )
 }
---
+++ zacierkasite/src/pages/LinkTree.tsx
@@ -0,0 +1,30 @@
+import { handleAuthentication } from '../lib/PocketBase';
+import { useSearchParams } from 'react-router-dom';
+import '../css/LinkTree.css'
+
+function LinkTree() {
+
+    const links = [
+        {
+            service_name: "Strava"
+        }
+    ]
+
+    const [searchParams] = useSearchParams();
+    const discordId: string | null = searchParams.get("discordId");    
+
+    if(discordId == null) {
+        return (
+            <h1>Invalid DiscordId Link</h1>
+        )
+    }
+
+    return (
+        <div className="LinkTree">
+            <h1 className='linktree-header'>EBoyClique Links</h1>
+            <button className='linktree-button' onClick={ async () => handleAuthentication(discordId)}>Authorize with {links[0].service_name}</button>
+        </div>
+    )
+}
+
+export default LinkTree;
